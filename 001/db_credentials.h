0e150a7dcaaebab58e4503d6aebad7a75441717a
HerrBaron <marco.schmitz@gmail.com>
2013-12-18 18:41:13 +0100

    initial checkin

---
+++ 001/db_credentials.h
@@ -0,0 +1,5 @@
+#define USER	"root"
+#define HOST	"nr10"
+#define PASS	""
+#define DB		"test"
+#define PORT	3306
---
+++ 001/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <windows.h> 
+#include <stdio.h> 
+#include <mysql.h> 
+
+#include "db_credentials.h"
+using namespace std;
+MYSQL mysql;
+MYSQL * conn;
+
+int main()
+{
+	mysql_init(&mysql);
+	
+	conn = mysql_real_connect (&mysql, HOST, USER, PASS, DB, PORT, 0, 0); 
+
+	if (conn == NULL)
+	{
+		cout << "Keine Verbindung zur Datenbank" << endl;
+	}
+	else
+	{
+		cout << "Erfolgreich verbunden" << endl;
+	}
+
+	system("pause");
+
+	mysql_close(conn);
+	
+	return 0;
+}
